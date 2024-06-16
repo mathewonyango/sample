a1b4dab7fe67b8e8b66a022f009a4ddc65c5ac09
Mathews Mc'Agumbah <mathewsagumbah@gmail.com>
2024-06-16 03:09:43 +0300

    routes

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/register', 'UsersController@create')->name('register.create');
+Route::post('/register', 'UsersController@store')->name('register.store');
