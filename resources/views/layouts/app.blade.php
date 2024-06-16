b79ad27c5a17b8ed721ec14abbc57726e1645dd7
Mathews Mc'Agumbah <mathewsagumbah@gmail.com>
2024-06-16 03:14:04 +0300

    second commit

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,6 +18,6 @@
         </div>
 
         <!-- Scripts -->
-        <script src="{{ mix('js/app.js') }}" defer></script>
+        {{-- <script src="{{ mix('js/app.js') }}" defer></script> --}}
     </body>
 </html>
